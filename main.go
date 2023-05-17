1c465f698ee38a3d1cd720a2b7e6b3d7c5655456
ltslts <netlts@163.com>
2023-05-17 15:44:39 +0800

    添加了清理channel的功能 重新命名了变量

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 	"os"
 )
 
-//电话队列长度设置为2
-var Phone_chan chan string = make(chan string, 2)
+// PhoneChan 电话队列长度设置为2000
+var PhoneChan chan string = make(chan string, 2000)
 
 func main() {
 	//读取配置文件
@@ -21,29 +21,44 @@ func main() {
 	}
 
 	// 初始化串口
-	serial_name := cfg.Section("").Key("serial").String() //  /dev/ttyS1
-	serial_baud, _ := cfg.Section("").Key("baud").Int()
-	call.Init(serial_name, serial_baud)
+	serialName := cfg.Section("").Key("serial").String() //  /dev/ttyS1
+	serialBaud, _ := cfg.Section("").Key("baud").Int()
+	call.Init(serialName, serialBaud)
 	defer call.Modem_Port.Close()
 
 	//开启打电话go协程
-	go go_call()
+	go goCall()
 
 	//初始化http服务
-	http_port := cfg.Section("").Key("http").String()
+	httpPort := cfg.Section("").Key("http").String()
 	router := gin.Default()
-	router.GET("/call", handler_call)
-	router.Run(":" + http_port)
+	router.GET("/call", handlerCall)
+	router.GET("/clean", handlerClean)
+
+	router.Run(":" + httpPort)
 }
 
-func go_call() {
-	for v := range Phone_chan {
+func goCall() {
+	for v := range PhoneChan {
 		call.Make_call(v)
+		//fmt.Printf("call=%s\n", v)
+		//time.Sleep(2 * time.Second)
 	}
 }
 
-//匹配的url格式:  /call?phone=138138138138&key=iamops
-func handler_call(c *gin.Context) {
+func handlerClean(c *gin.Context) {
+	var phones string
+	var countPhones string
+	count := len(PhoneChan)
+	for len(PhoneChan) > 0 {
+		phones = <-PhoneChan
+		countPhones = countPhones + phones + ","
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "clean=" + fmt.Sprintf("%d", count) + "|" + countPhones})
+}
+
+// 匹配的url格式:  /call?phone=138138138138&key=iamops
+func handlerCall(c *gin.Context) {
 	phone := c.Query("phone")
 	key := c.Query("key") // 是 c.Request.URL.Query().Get("lastname") 的简写
 
@@ -56,6 +71,6 @@ func handler_call(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "OK," + phone + key,
 		})
-		Phone_chan <- phone
+		PhoneChan <- phone
 	}
 }
